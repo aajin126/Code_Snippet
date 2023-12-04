ab3cd488a5daf89781c8fac2111b8d5f5faa5518
aajinlee <leehahj77@gmail.com>
2023-12-04 14:44:44 +0900

    Create BAEKJOON_3190.py

---
+++ Python/BAEKJOON_3190.py
@@ -0,0 +1,20 @@
+N = int(input())
+K = int(input())
+
+apple = []
+
+for i in range(K):
+    for j in range(1):
+        pos = list(map(int, input().split()))
+        apple.append(pos)
+
+L = int(input())
+
+time = []
+dir = []
+
+for k in range(L):
+    for j in range(1):
+        temp = list(map(input().split()))
+        time.append(int(temp[k][0]))
+        dir.append(temp[k][1])
